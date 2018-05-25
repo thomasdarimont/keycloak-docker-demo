d8aaf0655df7abef977aecea9271715329a1c65f
Thomas Darimont <thomas.darimont@gmail.com>
2018-05-25 19:47:22 +0100

    Fix version for keycloak docker-image

--- idm-system/keycloak/docker-build.sh
+++ idm-system/keycloak/docker-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-KC_VERSION=${KC_VERSION:-latest}
+#KC_VERSION=${KC_VERSION:-latest}
+KC_VERSION=${KC_VERSION:-3.4.3.Final.3.springio18}
 
 docker build -t tdlabs/keycloak:$KC_VERSION .
 
