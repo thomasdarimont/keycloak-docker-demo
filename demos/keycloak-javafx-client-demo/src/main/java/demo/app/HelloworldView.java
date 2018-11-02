06f88958ae9888a594ffacc416ab0dd0eaa26e0f
Thomas Darimont <thomas.darimont@gmail.com>
2018-11-02 13:39:48 +0100

    Remove old javafx client demo

--- demos/keycloak-javafx-client-demo/src/main/java/demo/app/HelloworldView.java
+++
@@ -1,9 +0,0 @@
-package demo.app;
-
-import de.felixroske.jfxsupport.AbstractFxmlView;
-import de.felixroske.jfxsupport.FXMLView;
-
-@FXMLView
-public class HelloworldView extends AbstractFxmlView {
-
-}
